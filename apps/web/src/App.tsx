2faf8a6722e274da10376a8910fdbcf7699fa401
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-01 00:58:16 +0200

    feat(apps/web): adding layout for home page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,100 +1,92 @@
 import { useQuery, gql } from '@apollo/client'
+import { Key } from 'react'
+import DeviceIcon from './icons/device-icon.svg'
+import SchoolIcon from './icons/school-icon.svg'
+import WarninglIcon from './icons/warning-icon.svg'
+import DetailslIcon from './icons/details-icon.svg'
+import './App.css'
 
 const GET_BATTERIES = gql`
-  query Batteries {
-    batteries {
+  query BatteriesIssues {
+    batteryIssues {
       academyId
-      batteryLevel
-      employeeId
-      id
-      serialNumber
-      timestamp
+      devices
+      totalProblems
     }
   }
 `
 
-interface Reading {
-  timestamp: string
-  batteryLevel: number
-}
-
-interface BatteryData {
-  academyId: number
-  batteryLevel: number
-  employeeId: string
-  id: string
-  serialNumber: string
-  timestamp: string
-}
-
-interface School {
-  totalProblems: number
-  devices: string[]
-}
-
 function App() {
   const { loading, error, data } = useQuery(GET_BATTERIES)
-  console.log(data, 'data')
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error : {error.message}</p>
 
-  function identificarProblemasDeBateria(
-    batteryData: BatteryData[]
-  ): { academyId: number; totalProblems: number; devices: string[] }[] {
-    const academies: {
-      [key: number]: { totalProblems: number; devices: Set<string> }
-    } = {}
-
-    for (const battery of batteryData) {
-      if (battery.batteryLevel < 0.3) {
-        if (!academies[battery.academyId]) {
-          academies[battery.academyId] = {
-            totalProblems: 0,
-            devices: new Set<string>(),
-          }
-        }
-
-        academies[battery.academyId].totalProblems++
-        academies[battery.academyId].devices.add(battery.serialNumber)
-      }
-    }
-
-    return Object.entries(academies).map(
-      ([academyId, { totalProblems, devices }]) => ({
-        academyId: Number(academyId),
-        totalProblems,
-        devices: Array.from(devices),
-      })
-    )
-  }
-
-  // // Exemplo de uso
-  // const batteryData: BatteryData[] = [
-  //   {
-  //     academyId: 30006,
-  //     batteryLevel: 0.74,
-  //     employeeId: 'T1007384',
-  //     id: 'be82ad1b-77c5-41d2-a88a-cd5f3fb343d0',
-  //     serialNumber: '1805C67HD02259',
-  //     timestamp: '2019-05-22T16:06:05.934Z',
-  //   },
-  //   {
-  //     academyId: 30006,
-  //     batteryLevel: 0.76,
-  //     employeeId: 'T1007384',
-  //     id: '313cd1f7-54a9-4799-a1bd-87d3d2f37365',
-  //     serialNumber: '1805C67HD02259',
-  //     timestamp: '2019-05-22T15:22:14.287Z',
-  //   },
-  // ]
-
-  const resultado = identificarProblemasDeBateria(data.batteries)
-  console.log(resultado)
+  console.log(data, 'data')
 
   return (
     <>
-      <h1>Hello world</h1>
+      <ul>
+        {data.batteryIssues.map(
+          (
+            report: {
+              academyId: string
+              devices: string[]
+              totalProblems: number
+            },
+            index: Key | null | undefined
+          ) => (
+            <li key={index} className="device-list">
+              <div className="device-list-wrapper">
+                <img src={SchoolIcon} alt="Academy icon" />
+                <div className="device-list-academy">
+                  <label className="device-list-academy-label">
+                    Academy Id
+                  </label>
+                  <div className="device-list-academy-value">
+                    {report.academyId}
+                  </div>
+                </div>
+              </div>
+              <div className="device-list-wrapper">
+                <img src={DeviceIcon} alt="Device icon" />
+                <div className="device-list-academy">
+                  <label className="device-list-academy-label">
+                    Device problem
+                  </label>
+                  <div className="device-list-academy-value">
+                    {report.devices.length}
+                  </div>
+                </div>
+              </div>
+              <div className="device-list-wrapper">
+                <img src={WarninglIcon} alt="Total problem icon" />
+                <div className="device-list-academy">
+                  <label className="device-list-academy-label">
+                    Total problem
+                  </label>
+                  <div className="device-list-academy-value">
+                    {report.totalProblems}
+                  </div>
+                </div>
+              </div>
+              <div className="device-list-wrapper">
+                <img src={DetailslIcon} alt="Total problem icon" />
+                <div className="device-list-academy">
+                  <div className="device-list-academy-value">More details</div>
+                </div>
+              </div>
+              {/* <div>
+                {report.devices.map((device, index: Key) => (
+                  <small key={index} className="device-list-item">
+                    <img src={DeviceIcon} alt="Device error icon" /> {device}
+                  </small>
+                ))}
+              </div> */}
+            </li>
+          )
+        )}
+      </ul>
     </>
   )
 }
