42ae84a3a19cc80930a625bcd1de22f28d37bd94
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-05-31 19:18:03 +0200

    chore(app/server): setup `type-graphql`

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,1 @@
-import { prisma } from '../prisma/client'
-
-async function main() {
-  const allBatteries = await prisma.battery.findMany()
-  console.log(allBatteries, 'battery')
-  console.log(allBatteries.length, 'battery.size')
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+import 'reflect-metadata'
